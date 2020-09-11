58d5dc94affc5c559769936c79506d003274a592
Adarsh Murthy <am4417@columbia.edu>
2020-09-11 12:20:37 -0400

    Update the fonts dir.

--- src/globals/typography.tsx
+++ src/globals/typography.tsx
@@ -2,35 +2,35 @@ const Typography = {
   '@font-face': [
     {
       fontFamily: 'Basier Square Regular',
-      src: `url(./fonts/basier-square/basiersquare-regular.eot),
-            url(./fonts/basier-square/basiersquare-regular.eot?#iefix) format('embedded-opentype'),
-            url(./fonts/basier-square/basiersquare-regular.woff) format('woff'),
-            url(./fonts/basier-square/basiersquare-regular.otf) format('opentype'),
-            url(./fonts/basier-square/basiersquare-regular.svg) format('svg')`
+      src: `url(../fonts/basier-square/basiersquare-regular.eot),
+            url(../fonts/basier-square/basiersquare-regular.eot?#iefix) format('embedded-opentype'),
+            url(../fonts/basier-square/basiersquare-regular.woff) format('woff'),
+            url(../fonts/basier-square/basiersquare-regular.otf) format('opentype'),
+            url(../fonts/basier-square/basiersquare-regular.svg) format('svg')`
     },
     {
       fontFamily: 'Basier Square Medium',
-      src: `url(./fonts/basier-square/basiersquare-medium.eot),
-            url(./fonts/basier-square/basiersquare-medium.eot?#iefix) format('embedded-opentype'),
-            url(./fonts/basier-square/basiersquare-medium.woff) format('woff'),
-            url(./fonts/basier-square/basiersquare-medium.otf) format('opentype'),
-            url(./fonts/basier-square/basiersquare-medium.svg) format('svg')`
+      src: `url(../fonts/basier-square/basiersquare-medium.eot),
+            url(../fonts/basier-square/basiersquare-medium.eot?#iefix) format('embedded-opentype'),
+            url(../fonts/basier-square/basiersquare-medium.woff) format('woff'),
+            url(../fonts/basier-square/basiersquare-medium.otf) format('opentype'),
+            url(../fonts/basier-square/basiersquare-medium.svg) format('svg')`
     },
     {
       fontFamily: 'Basier Square Bold',
-      src: `url(./fonts/basier-square/basiersquare-bold.eot),
-            url(./fonts/basier-square/basiersquare-bold.eot?#iefix) format('embedded-opentype'),
-            url(./fonts/basier-square/basiersquare-bold.woff) format('woff'),
-            url(./fonts/basier-square/basiersquare-bold.otf) format('opentype'),
-            url(./fonts/basier-square/basiersquare-bold.svg) format('svg')`
+      src: `url(../fonts/basier-square/basiersquare-bold.eot),
+            url(../fonts/basier-square/basiersquare-bold.eot?#iefix) format('embedded-opentype'),
+            url(../fonts/basier-square/basiersquare-bold.woff) format('woff'),
+            url(../fonts/basier-square/basiersquare-bold.otf) format('opentype'),
+            url(../fonts/basier-square/basiersquare-bold.svg) format('svg')`
     },
     {
       fontFamily: 'Basier Square SemiBold',
-      src: `url(./fonts/basier-square/basiersquare-semibold.eot),
-            url(./fonts/basier-square/basiersquare-semibold.eot?#iefix) format('embedded-opentype'),
-            url(./fonts/basier-square/basiersquare-semibold.woff) format('woff'),
-            url(./fonts/basier-square/basiersquare-semibold.otf) format('opentype'),
-            url(./fonts/basier-square/basiersquare-semibold.svg) format('svg')`
+      src: `url(../fonts/basier-square/basiersquare-semibold.eot),
+            url(../fonts/basier-square/basiersquare-semibold.eot?#iefix) format('embedded-opentype'),
+            url(../fonts/basier-square/basiersquare-semibold.woff) format('woff'),
+            url(../fonts/basier-square/basiersquare-semibold.otf) format('opentype'),
+            url(../fonts/basier-square/basiersquare-semibold.svg) format('svg')`
     }
   ]
 };
