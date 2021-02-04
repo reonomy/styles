9f00f22488c6a33eeaf7f48115fb3536a50f67c4
Victor DePina <depinav@ip-192-168-253-14.ec2.internal>
2021-02-04 17:04:51 -0500

    Fixed component names, added to storybook

---
+++ src/ionicons/outlineIcons/IconNdjsonOutline.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react/types-6-0';
+import { IconNdjsonOutline } from './IconNdjsonOutline';
+
+export default {
+  title: 'Icons/NDJSON',
+  component: IconNdjsonOutline
+} as Meta;
+
+export * from './IconNdjsonOutline';
