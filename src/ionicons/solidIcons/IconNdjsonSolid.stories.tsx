895474939fb3b983f75b602aa6b75c8143d8d8a1
vdepina <53443068+vdepina@users.noreply.github.com>
2021-02-08 12:56:23 -0500

    Adding outline and solid icons (#37)

---
+++ src/ionicons/solidIcons/IconNdjsonSolid.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react/types-6-0';
+import { IconNdjsonSolid } from './IconNdjsonSolid';
+
+export default {
+  title: 'Icons/NDJSON',
+  component: IconNdjsonSolid
+} as Meta;
+
+export * from './IconNdjsonSolid';
